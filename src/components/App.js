60cbc878cf9591331b9eef514959cdcc14a9b60a
thsvr <vr.ths.zd@gmail.com>
2020-04-20 16:34:03 -0300

    Implemented App component to render Display and Button Panel.

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
+// import PropTypes from 'prop-types';
+// import Big from 'big.mjs';
+
+const App = () => {
+  return (
+    <div>
+      <Display />
+      <ButtonPanel />
+    </div>
+  );
+};
+
+export default App;
