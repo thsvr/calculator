ec193aea9aa428adf4c1881f25f19fe628fc9d2a
thsvr <vr.ths.zd@gmail.com>
2020-04-22 16:51:18 -0300

    Add style: use flex display for App, Display and ButtonPanel components. Set width as 700px.

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,6 +3,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 // import Big from 'big.mjs';
 
+import '../index.css';
+
 const Button = ({ name }) => (
   <button type="submit" className="btnStyle">
     {name}
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
+import '../index.css';
 
 const ButtonPanel = ({ clickBtn }) => {
   const handleClick = buttonName => {
