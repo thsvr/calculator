eba74ee5127efe03ee4db277b4d1f06cc30a2219
thsvr <vr.ths.zd@gmail.com>
2020-05-08 17:32:55 -0300

    Import calculation function on App component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import Calculate from '../logic/calculate';
 // import PropTypes from 'prop-types';
 // import Big from 'big.mjs';
 
-const App = () => (
-  <div id="appId" className="app-class">
-    <Display />
-    <ButtonPanel />
-  </div>
-);
+const App = () => {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = buttonName => {
+    setState(Calculate(state, buttonName));
+  };
+
+  return (
+    <div id="appId" className="app-class">
+      <Display />
+      <ButtonPanel clickHandler={handleClick} />
+    </div>
+  );
+};
 
 export default App;
